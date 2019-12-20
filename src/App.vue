3b0eaf4b3c4b2d8eb1f99abeeb6b206ae34b0fc3
M. Hille <marciausgoslar@gmail.com>
2019-12-20 17:22:59 +0100

    Added todo edit mode + completion functionality

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,22 @@
   <div>
     <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo" />
     <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        @dblclick="toggleEditMode(todo)"
+        :class="{complete: todo.completed}"
+      >
+        <input
+          v-model="todo.label"
+          @keydown.enter="confirmChange(todo)"
+          v-if="todo.isEditable"
+          placeholder="New label"
+        />
         <h3>{{ todo.label }}</h3>
         <div>
           <button @click="removeTodo(todo.id)">🚮</button>
+          <button @click="completeTodo(todo)">✅</button>
         </div>
       </li>
     </ul>
@@ -25,16 +37,29 @@ export default {
       this.todos.push({
         id: this.todos.length,
         label: this.currentTodo,
-        completed: false
+        completed: false,
+        isEditable: false
       });
       this.currentTodo = "";
     },
     removeTodo(id) {
       this.todos.splice(id, 1);
+    },
+    toggleEditMode(todo) {
+      todo.isEditable = !todo.isEditable;
+    },
+    confirmChange(todo) {
+      todo.isEditable = false;
+    },
+    completeTodo(todo) {
+      todo.completed = true;
     }
   }
 };
 </script>
 
 <style>
+.complete {
+    background-color: aquamarine;
+}
 </style>
