bc4b43511f85eb315a2c609a176a9489504c942b
M. Hille <marciausgoslar@gmail.com>
2019-12-20 17:37:45 +0100

    Added black-green-dark theme

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import { MdField } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/black-green-dark.css';
+
 
 Vue.use(MdField);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
