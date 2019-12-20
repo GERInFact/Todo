49486372ed30d001834e085108905e8cc1dbed8b
M. Hille <marciausgoslar@gmail.com>
2019-12-20 17:01:40 +0100

    Added todo container and functionality + UI template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
+  <div>
+    <input placeholder="Add a todo" />
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">{{ todo.label }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false
+      });
+      this.currentTodo = "";
+    }
+  }
+};
 </script>
 
 <style>
